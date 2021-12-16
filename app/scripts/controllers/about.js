7ff54f6bcf907cbfc6121d5aafb0d456996d65f1
bvon95 <bvon95@naver.com>
2021-12-16 22:36:25 +0900

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name churchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the churchApp
+ */
+angular.module('churchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name churchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the churchApp
+ */
+angular.module('churchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
